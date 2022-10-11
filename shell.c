3d67ad6e89869d737eb4b4606581a80825a8787e
bl0nder <goelpalaash@gmail.com>
2022-10-11 17:19:47 +0530

    Splitting given input into command and flags (\n not handled yet)

--- shell.c
+++ shell.c
@@ -8,20 +8,29 @@ const int maxChar = 1000;
 
 void command(char cmd[]) {
 	fgets(cmd, maxChar, stdin);	//fgets is better than scanf since scanf stops reading input when it encouters whitespace
-	// char *test = strtok(cmd, " ");
+	char *split = strtok(cmd, " ");
+	char mainCmd[20];
+	char flags[20];
+	int flagCounter = 0;
 
 	// if (cmd[strlen(cmd)-1] == '\n') {
 	// 	cmd[strlen(cmd)-1] = '\0';
 	// }
 
-	printf("%d\n", strlen("                    "));
 
-	// while (test != NULL) {
-	// 	if ((*test) != '\n') {
-	// 		printf("%s ", test);
-	// 		test = strtok(NULL, " ");	
-	// 	}
-	// }
+	while (split != NULL) {
+		if (strlen(mainCmd) == 0) {
+			mainCmd = split;
+		}
+		else {
+			flag[counter] = split;
+			counter++;
+		}
+		split = strtok(NULL, " ");
+	}
+
+	printf("Main command: %s\n", mainCmd);
+	printf("Flags: %s\n", flags);
 }
 
 void shell() {

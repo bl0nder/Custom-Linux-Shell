4a9be14d440464c1c494dded330fba3fccf06de4
bl0nder <goelpalaash@gmail.com>
2022-10-11 16:36:32 +0530

    changed cmd to array of characters instead of array of pointers

--- shell.c
+++ shell.c
@@ -3,13 +3,13 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-void command(char* cmd[]) {
-	fgets(&cmd, sizeof(cmd), stdin);	//gets is better than scanf since scanf stops reading input when it encouters whitespace
+void command(char cmd[]) {
+	fgets(cmd, sizeof(cmd), stdin);	//gets is better than scanf since scanf stops reading input when it encouters whitespace
 	printf("Here is what you entered: %s", cmd);
 }
 
 void shell() {
-	char* cmd[10];	 
+	char cmd[100];	 
 	int counter = 0;
 	while (counter < 0) {
 		printf("bl0nderShell> ");

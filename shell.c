7b4f3867bd957ca0f21c56fbd55186875efea9a3
bl0nder <goelpalaash@gmail.com>
2022-10-23 16:32:26 +0530

    more tests

--- shell.c
+++ shell.c
@@ -183,7 +183,7 @@ void executeCommand(char* split[], int splitLen) {
 			printf("[!] Some error occurred while executing this command");
 		}
 		else if (pid == 0) {
-			execl("ls", "option1", NULL);
+			execl("./ls", "option1", "ls", "helllooo", NULL);
 		}
 		else {
 			wait(NULL);

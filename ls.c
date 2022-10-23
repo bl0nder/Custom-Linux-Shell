c5a577cd22b5fd1cfdfdf8468d807c10f4025481
bl0nder <goelpalaash@gmail.com>
2022-10-23 17:51:50 +0530

    .

--- ls.c
+++ ls.c
@@ -2,10 +2,10 @@
 #include <dirent.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv[]) {
-    
-
-    
+int main(int argc, char* argv[]) {    
+    for (int i=0; i<2; i++) {
+        printf("%s\n", argv[i]);
+    }
     printf("Hello world :)\n");
     return 0;
 }

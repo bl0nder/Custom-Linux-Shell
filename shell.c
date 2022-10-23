fe14fd112302886e8c148f3e353815b64ad8debb
bl0nder <goelpalaash@gmail.com>
2022-10-23 17:31:54 +0530

    .

--- shell.c
+++ shell.c
@@ -184,7 +184,7 @@ void executeCommand(char* split[], int splitLen) {
 			printf("[!] Some error occurred while executing this command");
 		}
 		else if (pid == 0) {
-			char* lsflag = flag1;
+			char* lsflag1 = flag1;
 			char* lsflag2 = flag2;
 			execl("./ls", lsflag1, lsflag2, NULL);
 		}

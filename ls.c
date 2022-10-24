5d632c3f943c69209f36eee89e2bb86431c35ec8
bl0nder <goelpalaash@gmail.com>
2022-10-24 16:32:15 +0530

    ???????????

--- ls.c
+++ ls.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <errno.h>
 #include <stdlib.h>
 
 int main(int argc, char* argv[]) {    
     
-    printf("%d\n", argc);
+    //No flags
+    if (!strcmp(argv[1], "\0") && !strcmp(argv[2], "\0")) {
+        errno = 0;
+        
+        DIR* directory;
+        struct dirent* readDir;
+        
+        directory = opendir(".");
+        int readDirectory = readdir_r(directory);
+
+
+        if (errno != 0) {
+            printf("Directory does not exist");
+            return -1;
+        }
+        else {
+            while (readDir) {
+                printf("%s ", readDir -> d_name);
+                readDir = readDir -> next;
+            }
+        }
+    }
 
     printf("Hello world :)\n");
     return 0;

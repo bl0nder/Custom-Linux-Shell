d97efeeac149d8b3e097fff866a8c14f8d0dd587
bl0nder <goelpalaash@gmail.com>
2022-10-25 16:53:10 +0530

    rm done i hope

--- rm.c
+++ rm.c
@@ -6,12 +6,35 @@
 
 int main(int argc, char* argv[]) {
     
-    // char* file = "";
+    const char* file = argv[2];
 
-    // if (!strcmp(argv[0], "-i")) {
+    if (!strcmp(argv[0], "-i")) {
+        printf("Remove file '%s' (y/n)?", file);
+        char[10] ans;
+        scanf(ans);
 
-    // }
+        if (!strcmp(ans, "y") || !strcmp(ans, "Y") || !strcmp(ans, "yes")) {
+            remove(file);
 
-    printf("%s %s %s %s", argv[0], argv[1], argv[2], argv[3]);
+            if (errno != 0) {
+                printf("[!] File %s does not exist\n", file);
+                exit(-1);
+            }
+
+        }
+    }
+
+    else if (!strcmp(argv[0], "-f")) {
+        remove(file);
+    }
+
+    else {
+        remove (file);
+        if (errno != 0) {
+            printf("[!] File %s does not exist\n", file);
+            exit(-1);
+        }
+    }
+    
     return 0;
 }

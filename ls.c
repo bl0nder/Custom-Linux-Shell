2cfac5e896531cd05078e4009631937f6910afd7
bl0nder <goelpalaash@gmail.com>
2022-10-25 13:27:37 +0530

    added -i flag

--- ls.c
+++ ls.c
@@ -3,6 +3,8 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 int main(int argc, char* argv[]) {    
 
@@ -14,15 +16,35 @@ int main(int argc, char* argv[]) {
     directory = opendir(".");
     readDir = readdir(directory);
 
+    struct stat fileStats;
 
     if (errno != 0) {
-        printf("Directory does not exist");
-        return -1;
+        printf("Directory does not exist\n");
+        exit(-1);
     }
     else {
+
         while (readDir != NULL) {
             if (readDir -> d_name[0] != '.') {
-                if (!strcmp(argv[0], "-m")) {
+                
+                if (!strcmp(argv[1], "-i")) {
+                    stat(readDir -> d_name, &fileStats);
+                    if (errno != 0) {
+                        printf ("[!] Could not retrieve file statistics\n");
+                        exit(-1);
+                    }
+                }
+
+                if (!strcmp(argv[0], "-m") && !strcmp(argv[1], "-i")) {
+                    if (readdir(directory) == NULL) {
+                        printf("%d %s", fileStats.st_ino, readDir -> d_name);    
+                    }
+                    else {
+                        printf("%d %s, ", fileStats.st_ino, readDir -> d_name);    
+                    }
+                }
+
+                else if (!strcmp(argv[0], "-m") && !strcmp(argv[1], "\0")) {
                     if (readdir(directory) == NULL) {
                         printf("%s", readDir -> d_name);    
                     }
@@ -30,6 +52,11 @@ int main(int argc, char* argv[]) {
                         printf("%s, ", readDir -> d_name);    
                     }
                 }
+
+                else if (!strcmp(argv[1], "\0") && !strcmp(argv[1], "-i")) {
+                    printf("%d %s ", fileStats.st_ino, readDir -> d_name);
+                }
+
                 else {
                     printf("%s ", readDir -> d_name);
                 }

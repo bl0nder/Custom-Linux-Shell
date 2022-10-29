806237afd57be87c47c874e43b93f01e9506b740
bl0nder <goelpalaash@gmail.com>
2022-10-29 16:08:22 +0530

    .

--- shell.c
+++ shell.c
@@ -399,9 +399,9 @@ int executeCommand(char* split[], int splitLen, char p[]) {
 				execl(strcat(p, "/mkdir"), "\0", "-p", fileName, modeArg, 0);
 			}
 			else {
-				printf("%s\n", p);
 				execl(strcat(p, "/mkdir"), "\0", "\0", fileName, modeArg, 0);
 			}
+			exit(0);
 		}
 
 		else {

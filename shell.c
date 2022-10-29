ad7162ece95a48fb189be83e84e480d0c6e0558c
bl0nder <goelpalaash@gmail.com>
2022-10-30 01:01:24 +0530

    testing pthread

--- shell.c
+++ shell.c
@@ -5,6 +5,7 @@
 #include <sys/wait.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
 
 const int maxChar = 1000;
 const int commandSize = 20;
@@ -107,312 +108,325 @@ int cd(char flag1[], char flag2[], char* argument[]) {
 	}
 }
 
+void* ls() {
+	printf("Thread created woohoo!\n");
+}
+
+int threadExecute(char* split[], int splitLen, char p[]) {
+	pthread_t t;
+
+	int test = pthread_create(&t, NULL, )
+	return 0;
+	
+}
 
 int executeCommand(char* split[], int splitLen, char p[]) {
 	
-	//Get command from split
-	char cmd[commandSize];
-	strcpy(cmd, split[0]);
+	threadExecute(split, splitLen, p);
 
-	//Strings to store flags
-	char flag1[flagSize];
-	char flag2[flagSize];
+	// //Get command from split
+	// char cmd[commandSize];
+	// strcpy(cmd, split[0]);
 
-	//Char pointer to store argument (could have multiple substrings in it)
-	char* argument[argSize];
+	// //Strings to store flags
+	// char flag1[flagSize];
+	// char flag2[flagSize];
 
-	//Initialise flag1 and flag2 to be arrays of null characters (helps in checking if flags exist or not)
-	for (int i=0; i<flagSize; i++) {
-		flag1[i] = '\0';
-		flag2[i] = '\0';
-	}
+	// //Char pointer to store argument (could have multiple substrings in it)
+	// char* argument[argSize];
 
-	//Initialise argument array to contain null 
-	for (int i=0; i<argSize; i++) {
-		argument[i] = NULL;
-	}
+	// //Initialise flag1 and flag2 to be arrays of null characters (helps in checking if flags exist or not)
+	// for (int i=0; i<flagSize; i++) {
+	// 	flag1[i] = '\0';
+	// 	flag2[i] = '\0';
+	// }
 
-	//Get flags from split and store them in flag1 and flag2
-	if (splitLen >= 2) {
-		if (split[1][0] == '-' && strlen(split[1])==2) {
-			strcpy(flag1, split[1]);
-		}
-		if (splitLen >= 3 && split[2][0] == '-' && strlen(split[2])==2) {
-			strcpy(flag2, split[2]);
-		}
-	}
+	// //Initialise argument array to contain null 
+	// for (int i=0; i<argSize; i++) {
+	// 	argument[i] = NULL;
+	// }
 
-	//set argument based on how many flags there are
-	int start = 0;
-	if (flag1[0] == '\0' && flag2[0] == '\0') {
-		start = 1;
-	}
-	else if (flag1[0] != '\0' && flag2[0]=='\0') {
-		start = 2;
-	}
-	else if (flag1[0] != '\0' && flag2[0] !='\0') {
-		start = 3;
-	}
+	// //Get flags from split and store them in flag1 and flag2
+	// if (splitLen >= 2) {
+	// 	if (split[1][0] == '-' && strlen(split[1])==2) {
+	// 		strcpy(flag1, split[1]);
+	// 	}
+	// 	if (splitLen >= 3 && split[2][0] == '-' && strlen(split[2])==2) {
+	// 		strcpy(flag2, split[2]);
+	// 	}
+	// }
 
-	int counter = 0;
-	for (int i=start; i<splitLen; i++) {
-		argument[counter] = split[i];
-		counter++;
-	}
+	// //set argument based on how many flags there are
+	// int start = 0;
+	// if (flag1[0] == '\0' && flag2[0] == '\0') {
+	// 	start = 1;
+	// }
+	// else if (flag1[0] != '\0' && flag2[0]=='\0') {
+	// 	start = 2;
+	// }
+	// else if (flag1[0] != '\0' && flag2[0] !='\0') {
+	// 	start = 3;
+	// }
 
-	//Exit shell
-	if (!strcmp(cmd, "exit")) {
-		exit(0);
-	}
+	// int counter = 0;
+	// for (int i=start; i<splitLen; i++) {
+	// 	argument[counter] = split[i];
+	// 	counter++;
+	// }
 
-	//cd - change directory
-	else if (!strcmp(cmd, "cd")) {
-		cd(flag1, flag2, argument);
-	}
+	// //Exit shell
+	// if (!strcmp(cmd, "exit")) {
+	// 	exit(0);
+	// }
 
-	//pwd - print working directory
-	else if (!strcmp(cmd, "pwd")) {
-		pwd(flag1, flag2);
-	}
+	// //cd - change directory
+	// else if (!strcmp(cmd, "cd")) {
+	// 	cd(flag1, flag2, argument);
+	// }
 
-	//echo 
-	else if (!strcmp(cmd, "echo")) {
-		echo(flag1, flag2, argument);
-	}
+	// //pwd - print working directory
+	// else if (!strcmp(cmd, "pwd")) {
+	// 	pwd(flag1, flag2);
+	// }
 
-	else if (!strcmp(cmd, "ls")) {
+	// //echo 
+	// else if (!strcmp(cmd, "echo")) {
+	// 	echo(flag1, flag2, argument);
+	// }
+
+	// else if (!strcmp(cmd, "ls")) {
 		
-		//Flags
-		int m = 0;
-		int i = 0;
+	// 	//Flags
+	// 	int m = 0;
+	// 	int i = 0;
 
-		//-m flag
-		if (!strcmp(flag1, "-m") || !strcmp(flag2, "-m")) {
-			m = 1;
-		}
+	// 	//-m flag
+	// 	if (!strcmp(flag1, "-m") || !strcmp(flag2, "-m")) {
+	// 		m = 1;
+	// 	}
 
-		//-i flag
-		if (!strcmp(flag1, "-i") || !strcmp(flag2, "-i")) {
-			i = 1;
-		}
+	// 	//-i flag
+	// 	if (!strcmp(flag1, "-i") || !strcmp(flag2, "-i")) {
+	// 		i = 1;
+	// 	}
 
-		char tempDir[100];
-		getcwd(tempDir, 100);
+	// 	char tempDir[100];
+	// 	getcwd(tempDir, 100);
 
-		const char* dir = tempDir;
+	// 	const char* dir = tempDir;
 		
-		pid_t pid;
-		pid = fork();
+	// 	pid_t pid;
+	// 	pid = fork();
 
-		if (pid < 0) {
-			printf("[!] Some error occurred while executing this command");
-		}
-		else if (pid == 0) {
-			if (m && i) {
-				execl(strcat(p, "/ls"), "-m", "-i", dir, 0);
-			}
-			else if (m && !i) {
-				execl(strcat(p, "/ls"), "-m", "\0", dir, 0);
-			}
-			else if (!m && i) {
-				execl(strcat(p, "/ls"), "\0", "-i", dir, 0);
-			}
-			else if (!m && !i) {
-				execl(strcat(p, "/ls"), "\0", "\0", dir, 0);
-			}
-			else {
-				printf("[!] Invalid flag(s) entered.\n");
-				exit(-1);
-			}
+	// 	if (pid < 0) {
+	// 		printf("[!] Some error occurred while executing this command");
+	// 	}
+	// 	else if (pid == 0) {
+	// 		if (m && i) {
+	// 			execl(strcat(p, "/ls"), "-m", "-i", dir, 0);
+	// 		}
+	// 		else if (m && !i) {
+	// 			execl(strcat(p, "/ls"), "-m", "\0", dir, 0);
+	// 		}
+	// 		else if (!m && i) {
+	// 			execl(strcat(p, "/ls"), "\0", "-i", dir, 0);
+	// 		}
+	// 		else if (!m && !i) {
+	// 			execl(strcat(p, "/ls"), "\0", "\0", dir, 0);
+	// 		}
+	// 		else {
+	// 			printf("[!] Invalid flag(s) entered.\n");
+	// 			exit(-1);
+	// 		}
 
-			exit(0);
-		}
-		else {
-			wait(NULL);
-		}
-	}
+	// 		exit(0);
+	// 	}
+	// 	else {
+	// 		wait(NULL);
+	// 	}
+	// }
 
-	else if (!strcmp(cmd, "date")) {
+	// else if (!strcmp(cmd, "date")) {
 
-		int u = 0;
-		int r = 0;
+	// 	int u = 0;
+	// 	int r = 0;
 
-		if (!strcmp(flag1, "-u") || !strcmp(flag2, "-u")) {
-			u = 1;
-		}
+	// 	if (!strcmp(flag1, "-u") || !strcmp(flag2, "-u")) {
+	// 		u = 1;
+	// 	}
 
-		if (!strcmp(flag1, "-R") || !strcmp(flag2, "-R")) {
-			r = 1;
-		}
+	// 	if (!strcmp(flag1, "-R") || !strcmp(flag2, "-R")) {
+	// 		r = 1;
+	// 	}
 
-		if (strcmp(flag1, "\0") && strcmp(flag1, "-u") && strcmp(flag1, "-R")) {
-			printf("[!] Invalid flag(s) entered.\n");
-			return -1;
-		}
-		else if (strcmp(flag2, "\0") && strcmp(flag2, "-u") && strcmp(flag2, "-R")) {
-			return -1;
-		}
+	// 	if (strcmp(flag1, "\0") && strcmp(flag1, "-u") && strcmp(flag1, "-R")) {
+	// 		printf("[!] Invalid flag(s) entered.\n");
+	// 		return -1;
+	// 	}
+	// 	else if (strcmp(flag2, "\0") && strcmp(flag2, "-u") && strcmp(flag2, "-R")) {
+	// 		return -1;
+	// 	}
 
-		pid_t pid;
-		pid = fork();
+	// 	pid_t pid;
+	// 	pid = fork();
 
-		if (pid < 0) {
-			printf("[!] Some error occurred while executing this command\n");
-		}
-		else if (pid == 0) {
-			if (u && r) {
-				execl(strcat(p, "/date"), "-u", "-R", 0);
-			}
-			else if (u && !r) {
-				execl(strcat(p, "/date"), "-u", "\0", 0);
-			}
-			else if (r && !u) {
-				execl(strcat(p, "/date"), "\0", "-R", 0);
-			}
-			else if (!r && !u) {
-				execl(strcat(p, "/date"), "\0", "\0", 0);
-			}
+	// 	if (pid < 0) {
+	// 		printf("[!] Some error occurred while executing this command\n");
+	// 	}
+	// 	else if (pid == 0) {
+	// 		if (u && r) {
+	// 			execl(strcat(p, "/date"), "-u", "-R", 0);
+	// 		}
+	// 		else if (u && !r) {
+	// 			execl(strcat(p, "/date"), "-u", "\0", 0);
+	// 		}
+	// 		else if (r && !u) {
+	// 			execl(strcat(p, "/date"), "\0", "-R", 0);
+	// 		}
+	// 		else if (!r && !u) {
+	// 			execl(strcat(p, "/date"), "\0", "\0", 0);
+	// 		}
 
-		}
-		else {
-			wait(NULL);
-		}
-	}
+	// 	}
+	// 	else {
+	// 		wait(NULL);
+	// 	}
+	// }
 
-	else if (!strcmp(cmd, "rm")) {
-		int f = 0;
-		int i = 0;
+	// else if (!strcmp(cmd, "rm")) {
+	// 	int f = 0;
+	// 	int i = 0;
 
-		const char* fileName = argument[0];
+	// 	const char* fileName = argument[0];
 
-		if (!strcmp(flag1, "-i") || !strcmp(flag2, "-i")) {
-			i = 1;
-		}
+	// 	if (!strcmp(flag1, "-i") || !strcmp(flag2, "-i")) {
+	// 		i = 1;
+	// 	}
 
-		if (!strcmp(flag1, "-f") || !strcmp(flag2, "-f")) {
-			f = 1;
-		}
+	// 	if (!strcmp(flag1, "-f") || !strcmp(flag2, "-f")) {
+	// 		f = 1;
+	// 	}
 
-		pid_t pid;
-		pid = fork();
+	// 	pid_t pid;
+	// 	pid = fork();
 
-		if (pid < 0) {
-			printf("[!] Some error occurred while executing this command\n");
-		}
-		else if (pid == 0) {
-			if (f && i) {
-				execl(strcat(p, "/rm"), "-f", "-i", fileName, 0);
-			}
-			else if (f && !i) {
-				execl(strcat(p, "/rm"), "-f", "\0", fileName, 0);
-			}
-			else if (i && !f) {
-				execl(strcat(p, "/rm"), "\0", "-i", fileName, 0);
-			}
-			else {
-				execl(strcat(p, "/rm"), "\0", "\0", fileName, 0);
-			}
-		}
-		else {
-			wait(NULL);
-		}
-
-
-	}
-
-	else if (!strcmp(cmd, "cat")) {
-
-		int e = 0;
-		int t = 0;
+	// 	if (pid < 0) {
+	// 		printf("[!] Some error occurred while executing this command\n");
+	// 	}
+	// 	else if (pid == 0) {
+	// 		if (f && i) {
+	// 			execl(strcat(p, "/rm"), "-f", "-i", fileName, 0);
+	// 		}
+	// 		else if (f && !i) {
+	// 			execl(strcat(p, "/rm"), "-f", "\0", fileName, 0);
+	// 		}
+	// 		else if (i && !f) {
+	// 			execl(strcat(p, "/rm"), "\0", "-i", fileName, 0);
+	// 		}
+	// 		else {
+	// 			execl(strcat(p, "/rm"), "\0", "\0", fileName, 0);
+	// 		}
+	// 	}
+	// 	else {
+	// 		wait(NULL);
+	// 	}
 
 
-		if (!strcmp(flag1, "-E") || !strcmp(flag2, "-E")) {
-			e = 1;
-		}
-		if (!strcmp(flag1, "-T") || !strcmp(flag2, "-T")) {
-			t = 1;
-		}
+	// }
 
-		const char* fileName = argument[0];
+	// else if (!strcmp(cmd, "cat")) {
+
+	// 	int e = 0;
+	// 	int t = 0;
+
+
+	// 	if (!strcmp(flag1, "-E") || !strcmp(flag2, "-E")) {
+	// 		e = 1;
+	// 	}
+	// 	if (!strcmp(flag1, "-T") || !strcmp(flag2, "-T")) {
+	// 		t = 1;
+	// 	}
+
+	// 	const char* fileName = argument[0];
 		
-		pid_t pid;
-		pid = fork();
+	// 	pid_t pid;
+	// 	pid = fork();
 
-		if (pid < 0) {
-			printf("[!] Some error occurred while executing this command\n");
-			return -1;
-		}
+	// 	if (pid < 0) {
+	// 		printf("[!] Some error occurred while executing this command\n");
+	// 		return -1;
+	// 	}
 
-		else if (pid == 0) {
-			if (e && t) {
-				execl(strcat(p, "/cat"), "-E", "-T", fileName, 0);
-			}
-			else if (e && !t) {
-				execl(strcat(p, "/cat"), "-E", "\0", fileName, 0);
-			}
-			else if (!e && t) {
-				execl(strcat(p, "/cat"), "\0", "-T", fileName, 0);
-			}
-			else {
-				execl(strcat(p, "/cat"), "\0", "\0", fileName, 0);
-			}
-		}
+	// 	else if (pid == 0) {
+	// 		if (e && t) {
+	// 			execl(strcat(p, "/cat"), "-E", "-T", fileName, 0);
+	// 		}
+	// 		else if (e && !t) {
+	// 			execl(strcat(p, "/cat"), "-E", "\0", fileName, 0);
+	// 		}
+	// 		else if (!e && t) {
+	// 			execl(strcat(p, "/cat"), "\0", "-T", fileName, 0);
+	// 		}
+	// 		else {
+	// 			execl(strcat(p, "/cat"), "\0", "\0", fileName, 0);
+	// 		}
+	// 	}
 
-		else {
-			wait (NULL);
-		}
-	}
+	// 	else {
+	// 		wait (NULL);
+	// 	}
+	// }
 
-	else if (!strcmp(cmd, "mkdir")) {
-		int m = 0;
-		int pFlag = 0;
+	// else if (!strcmp(cmd, "mkdir")) {
+	// 	int m = 0;
+	// 	int pFlag = 0;
 
 
 
-		if (!strcmp(flag1, "-m") || !strcmp(flag2, "-m")) {
-			m = 1;
-		}
-		if (!strcmp(flag1, "-p") || !strcmp(flag2, "-p")) {
-			pFlag = 1;
-		}
+	// 	if (!strcmp(flag1, "-m") || !strcmp(flag2, "-m")) {
+	// 		m = 1;
+	// 	}
+	// 	if (!strcmp(flag1, "-p") || !strcmp(flag2, "-p")) {
+	// 		pFlag = 1;
+	// 	}
 
-		const char* fileName = argument[0];		
+	// 	const char* fileName = argument[0];		
 
-		const char* modeArg = "777";
+	// 	const char* modeArg = "777";
 		
-		pid_t pid;
-		pid = fork();
+	// 	pid_t pid;
+	// 	pid = fork();
 
-		if (pid < 0) {
-			printf("[!] Some error occurred while executing this command\n");
-			return -1;
-		}
+	// 	if (pid < 0) {
+	// 		printf("[!] Some error occurred while executing this command\n");
+	// 		return -1;
+	// 	}
 
-		else if (pid == 0) {
-			if (m && pFlag) {
-				execl(strcat(p, "/mkdir"), "-m", "-p", fileName, modeArg, 0);
-			}
-			else if (m && !pFlag) {
-				execl(strcat(p, "/mkdir"), "-m", "\0", fileName, modeArg, 0);
-			}
-			else if (!m && pFlag) {
-				execl(strcat(p, "/mkdir"), "\0", "-p", fileName, "777", 0);
-			}
-			else {
-				execl(strcat(p, "/mkdir"), "\0", "\0", fileName, "777", 0);
-			}
-			exit(0);
-		}
+	// 	else if (pid == 0) {
+	// 		if (m && pFlag) {
+	// 			execl(strcat(p, "/mkdir"), "-m", "-p", fileName, modeArg, 0);
+	// 		}
+	// 		else if (m && !pFlag) {
+	// 			execl(strcat(p, "/mkdir"), "-m", "\0", fileName, modeArg, 0);
+	// 		}
+	// 		else if (!m && pFlag) {
+	// 			execl(strcat(p, "/mkdir"), "\0", "-p", fileName, "777", 0);
+	// 		}
+	// 		else {
+	// 			execl(strcat(p, "/mkdir"), "\0", "\0", fileName, "777", 0);
+	// 		}
+	// 		exit(0);
+	// 	}
 
-		else {
-			wait (NULL);
-		}
-	}
+	// 	else {
+	// 		wait (NULL);
+	// 	}
+	// }
 
-	//unknown command
-	else {
-		printf("[!] Unknown command %s entered\n", cmd);
-	}
+	// //unknown command
+	// else {
+	// 	printf("[!] Unknown command %s entered\n", cmd);
+	// }
 
 	return 0;
 }
@@ -437,19 +451,6 @@ void command(char cmd[], char p[]) {
 		splitCounter++;
 	}
 
-
-	// char mainCmd[100] = "\0";
-
-	// //Removing trailing newline character
-	// if (strlen(cmd) > 0 && cmd[strlen(cmd)-1] == '\n') {
-	// 	cmd[strlen(cmd)-1] = '\0';
-	// }
-
-	// if (strlen(mainCmd) == 0) {
-	// 	strcpy(mainCmd, split);
-	// }
-
-	// split = strtok(NULL, " ");
 	executeCommand(split, splitLen, p);
 }
 

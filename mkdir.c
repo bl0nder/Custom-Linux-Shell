36ac2d00852ec5b26d1a8f135c10e560fff6f723
bl0nder <goelpalaash@gmail.com>
2022-10-31 00:11:35 +0530

    .

--- mkdir.c
+++ mkdir.c
@@ -10,7 +10,7 @@ int main (int argc, char* argv[]) {
 
     //No flags
     if (!strcmp(argv[1], "\0") && !strcmp(argv[2], "\0")) {
-        int createDir = mkdir(argv[2], S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+        int createDir = mkdir(argv[3], S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
 
         if (createDir!=0) {
             printf("[!] There was an error in creating directory '%s'.\n", argv[3]);

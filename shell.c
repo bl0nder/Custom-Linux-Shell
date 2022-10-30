bfe61b05989f58e8ffa5f3e06af7e2c70a7f63e7
bl0nder <goelpalaash@gmail.com>
2022-10-30 15:37:09 +0530

    .

--- shell.c
+++ shell.c
@@ -131,7 +131,7 @@ void* ls(void* passArgs) {
 	// 	strcat(str, " ");
 	// }
 
-	int check = snprintf(str, 2000, "%s %s %s %s", strcat(((struct args*)passArgs -> path), "/ls"), ((struct args*)passArgs) -> flag1, ((struct args*)passArgs) -> flag2, ((struct args*)passArgs) -> argument);
+	int check = snprintf(str, 2000, "%s %s %s %s", strcat(((struct args*)passArgs) -> path, "/ls"), ((struct args*)passArgs) -> flag1, ((struct args*)passArgs) -> flag2, ((struct args*)passArgs) -> argument);
 	
 	system((const char*) str);
 	printf("Thread created woohoo!\n");

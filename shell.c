a3fd320a20c87e6b57bdcea8bd3f245319eace26
bl0nder <goelpalaash@gmail.com>
2022-10-30 18:00:28 +0530

    .

--- shell.c
+++ shell.c
@@ -238,8 +238,8 @@ int executeCommand(char* split[], int splitLen, char p[]) {
 		counter++;
 	}
 
-	char* f1 = (char*)malloc(sizeof(char));
-	char* f2 = (char*)malloc(sizeof(char));
+	char* f1 = (char*)malloc(flagSize*sizeof(char));
+	char* f2 = (char*)malloc(flagSize*sizeof(char));
 
 	if (flag1[0] != '\0') {
 		strcpy(f1, flag1);
